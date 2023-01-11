46c31f7c386009ea1c4c8712202e132a3e5629c8
Jubaer Islam Nahin <105843586+JubaerNahin@users.noreply.github.com>
2023-01-11 15:55:12 +0600

    Add files via upload

---
+++ 1098.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    float i,j;
+    for(i=0;i<2.1;i+=0.2){
+      for(j=1;j<=3;j++){
+        cout<<"I="<<i<<" "<<"J="<<i+j<<endl;
+      }
+    }
+}

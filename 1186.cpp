aaf8b7d515cd8203a62fa207a746820cfbab024a
Jubaer Islam Nahin <105843586+JubaerNahin@users.noreply.github.com>
2023-01-13 11:06:11 +0600

    Add files via upload

---
+++ 1186.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{    
+    char T;
+    cin>>T;
+    float arr[12][12],count=0,tot=0,avg=0;
+    for(int i=0;i<12;i++){
+      for(int j=0;j<12;j++){
+        cin>>arr[i][j];  
+      }
+    
+    }
+    
+     for(int i=1;i<=11;i++){
+      for(int j=11;j>11-i;j--){
+        tot++;
+        count+=arr[i][j];  
+      }
+        
+     }
+     if(T=='S'){
+        printf("%.1f\n",count);
+     }
+      else{
+        avg=count/tot;
+        printf("%.1f\n",avg);
+      }
+}
